f621700e7e30b66442621e5548d6803616d9205a
akella <akella.a@gmail.com>
2017-03-06 10:47:19 +0200

    example fix

--- dist/easygooglemaps.js
+++ dist/easygooglemaps.js
@@ -65,18 +65,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /**
 	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * EasyGoogleMaps
 	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * Description
-	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      *
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * Creates map with expandable markers
 	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @name EasyGoogleMaps
-	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @function
-	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @param {Array} data An array of data
-	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @param {Object} options An object containing the following fields:
-	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      *
-	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * @return {Array} Result
 	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      */
 
 	var _underscore = __webpack_require__(1);
 
-	var _googleMaps = __webpack_require__(2);
+	var _googleMaps = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"google-maps\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 
 	var _googleMaps2 = _interopRequireDefault(_googleMaps);
 
@@ -111,7 +106,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        _googleMaps2.default.KEY = props.map.APIKEY;
 	        _googleMaps2.default.load(function (google) {
 
-	          !/* require */(/* min-size */function() { var __WEBPACK_AMD_REQUIRE_ARRAY__ = [__webpack_require__(3)]; (function (InfoBox) {
+	          !/* require */(/* min-size */function() { var __WEBPACK_AMD_REQUIRE_ARRAY__ = [__webpack_require__(2)]; (function (InfoBox) {
 	            that._initMap();
 	            //if you want to get info from some another file using ajax
 	            //you need turn on 'ajax' by flag in settings => ajax: true
@@ -1996,231 +1991,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 /* 2 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;(function(root, factory) {
-
-		if (root === null) {
-			throw new Error('Google-maps package can be used only in browser');
-		}
-
-		if (true) {
-			!(__WEBPACK_AMD_DEFINE_FACTORY__ = (factory), __WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ? (__WEBPACK_AMD_DEFINE_FACTORY__.call(exports, __webpack_require__, exports, module)) : __WEBPACK_AMD_DEFINE_FACTORY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
-		} else if (typeof exports === 'object') {
-			module.exports = factory();
-		} else {
-			root.GoogleMapsLoader = factory();
-		}
-
-	})(typeof window !== 'undefined' ? window : null, function() {
-
-
-		'use strict';
-
-
-		var googleVersion = '3.18';
-
-		var script = null;
-
-		var google = null;
-
-		var loading = false;
-
-		var callbacks = [];
-
-		var onLoadEvents = [];
-
-		var originalCreateLoaderMethod = null;
-
-
-		var GoogleMapsLoader = {};
-
-
-		GoogleMapsLoader.URL = 'https://maps.googleapis.com/maps/api/js';
-
-		GoogleMapsLoader.KEY = null;
-
-		GoogleMapsLoader.LIBRARIES = [];
-
-		GoogleMapsLoader.CLIENT = null;
-
-		GoogleMapsLoader.CHANNEL = null;
-
-		GoogleMapsLoader.LANGUAGE = null;
-
-		GoogleMapsLoader.REGION = null;
-
-		GoogleMapsLoader.VERSION = googleVersion;
-
-		GoogleMapsLoader.WINDOW_CALLBACK_NAME = '__google_maps_api_provider_initializator__';
-
-
-		GoogleMapsLoader._googleMockApiObject = {};
-
-
-		GoogleMapsLoader.load = function(fn) {
-			if (google === null) {
-				if (loading === true) {
-					if (fn) {
-						callbacks.push(fn);
-					}
-				} else {
-					loading = true;
-
-					window[GoogleMapsLoader.WINDOW_CALLBACK_NAME] = function() {
-						ready(fn);
-					};
-
-					GoogleMapsLoader.createLoader();
-				}
-			} else if (fn) {
-				fn(google);
-			}
-		};
-
-
-		GoogleMapsLoader.createLoader = function() {
-			script = document.createElement('script');
-			script.type = 'text/javascript';
-			script.src = GoogleMapsLoader.createUrl();
-
-			document.body.appendChild(script);
-		};
-
-
-		GoogleMapsLoader.isLoaded = function() {
-			return google !== null;
-		};
-
-
-		GoogleMapsLoader.createUrl = function() {
-			var url = GoogleMapsLoader.URL;
-
-			url += '?callback=' + GoogleMapsLoader.WINDOW_CALLBACK_NAME;
-
-			if (GoogleMapsLoader.KEY) {
-				url += '&key=' + GoogleMapsLoader.KEY;
-			}
-
-			if (GoogleMapsLoader.LIBRARIES.length > 0) {
-				url += '&libraries=' + GoogleMapsLoader.LIBRARIES.join(',');
-			}
-
-			if (GoogleMapsLoader.CLIENT) {
-				url += '&client=' + GoogleMapsLoader.CLIENT + '&v=' + GoogleMapsLoader.VERSION;
-			}
-
-			if (GoogleMapsLoader.CHANNEL) {
-				url += '&channel=' + GoogleMapsLoader.CHANNEL;
-			}
-
-			if (GoogleMapsLoader.LANGUAGE) {
-				url += '&language=' + GoogleMapsLoader.LANGUAGE;
-			}
-
-			if (GoogleMapsLoader.REGION) {
-				url += '&region=' + GoogleMapsLoader.REGION;
-			}
-
-			return url;
-		};
-
-
-		GoogleMapsLoader.release = function(fn) {
-			var release = function() {
-				GoogleMapsLoader.KEY = null;
-				GoogleMapsLoader.LIBRARIES = [];
-				GoogleMapsLoader.CLIENT = null;
-				GoogleMapsLoader.CHANNEL = null;
-				GoogleMapsLoader.LANGUAGE = null;
-				GoogleMapsLoader.REGION = null;
-				GoogleMapsLoader.VERSION = googleVersion;
-
-				google = null;
-				loading = false;
-				callbacks = [];
-				onLoadEvents = [];
-
-				if (typeof window.google !== 'undefined') {
-					delete window.google;
-				}
-
-				if (typeof window[GoogleMapsLoader.WINDOW_CALLBACK_NAME] !== 'undefined') {
-					delete window[GoogleMapsLoader.WINDOW_CALLBACK_NAME];
-				}
-
-				if (originalCreateLoaderMethod !== null) {
-					GoogleMapsLoader.createLoader = originalCreateLoaderMethod;
-					originalCreateLoaderMethod = null;
-				}
-
-				if (script !== null) {
-					script.parentElement.removeChild(script);
-					script = null;
-				}
-
-				if (fn) {
-					fn();
-				}
-			};
-
-			if (loading) {
-				GoogleMapsLoader.load(function() {
-					release();
-				});
-			} else {
-				release();
-			}
-		};
-
-
-		GoogleMapsLoader.onLoad = function(fn) {
-			onLoadEvents.push(fn);
-		};
-
-
-		GoogleMapsLoader.makeMock = function() {
-			originalCreateLoaderMethod = GoogleMapsLoader.createLoader;
-
-			GoogleMapsLoader.createLoader = function() {
-				window.google = GoogleMapsLoader._googleMockApiObject;
-				window[GoogleMapsLoader.WINDOW_CALLBACK_NAME]();
-			};
-		};
-
-
-		var ready = function(fn) {
-			var i;
-
-			loading = false;
-
-			if (google === null) {
-				google = window.google;
-			}
-
-			for (i = 0; i < onLoadEvents.length; i++) {
-				onLoadEvents[i](google);
-			}
-
-			if (fn) {
-				fn(google);
-			}
-
-			for (i = 0; i < callbacks.length; i++) {
-				callbacks[i](google);
-			}
-
-			callbacks = [];
-		};
-
-
-		return GoogleMapsLoader;
-
-	});
-
-
-/***/ },
-/* 3 */
 /***/ function(module, exports) {
 
 	/**
